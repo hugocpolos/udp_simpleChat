8bfdd0062c7eb1b26bd405fbd2d8c66646df13e7
Hugo Constantinopolos <hugo.constantinopolos@gruporbs.com.br>
2018-07-24 15:12:35 -0300

    documentation to server source file added.

--- server.py
+++ server.py
@@ -1,19 +1,39 @@
+#####################################
+# 
+# Chat UDP - Aplicação servidor
+# Autor: Hugo Constantinopolos.
+# Disponível em : https://github.com/hugocpolos/udp_simpleChat
+#
+#####################################
+
+#####################################
+# ÁREA DE IMPORT
 import socket
 import _thread
 from random import randint
+#####################################
 
-clientes = []
-HOST = input("Enter host address: ")
-PORT = 5000
-
+#####################################
+# Função para enviar uma mensagem para
+# todos os outros clientes.
+#
 def post_message(socket,message):   #Função para enviar uma mensagem para todos
     for cliente in clientes:        #os clientes. 
         print("Enviando "+ message + " para ")
         print(cliente)
         socket.sendto(message.encode(),cliente)
+#
+#####################################
 
-def Cliente_Thread(cliente):    #Thread para atender cada cliente, é criada uma para
-    new_bind = False            #cada cliente
+#####################################
+# Thread para tratamento de um cliente
+# O servidor roda uma para cada
+# cliente conectado.
+#
+#
+def Cliente_Thread(cliente):
+    # Inicialização das variáveis locais.
+    new_bind = False
     new_host = HOST
     new_port = randint(10000, 64000)
     message = ""
@@ -23,6 +43,9 @@ def Cliente_Thread(cliente):    #Thread para atender cada cliente, é criada uma
     #Abre um novo socket para tratar o cliente.
     thread_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+    # Laço para realizar o bind na nova porta gerada aleatoriamente,
+    # enquanto a porta estiver ocupada, será gerada uma nova porta
+    # e realizado nova tentativa de bind.
     while(new_bind == False):
         try:
             #print("Tentando o bind na porta %d\n" % new_port)
@@ -33,7 +56,6 @@ def Cliente_Thread(cliente):    #Thread para atender cada cliente, é criada uma
             new_port = randint(10000, 64000)
             #print ("criando nova porta %d \n" % new_port)
 
-    #print("Novo socket aberto\n")
     
     # Envia a nova porta para o cliente
     message = "NEW_PORT".encode()
@@ -47,31 +69,53 @@ def Cliente_Thread(cliente):    #Thread para atender cada cliente, é criada uma
     message = "Nome de usuário: ".encode()
     thread_sock.sendto(message,cliente)
 
-    #Espera a resposta
+    #Espera a resposta com o nome de usuário.
     msg, cliente = thread_sock.recvfrom(1024)
     user_name = msg.decode()
 
-    #Trata mensagens do cliente
-    #print ("Conexão estabelecida com o cliente.\n")
+    # Nesse momento o cliente está conectado.
+    # é enviado a mensagem "Usuário entrou."
+    # para os clientes.
     user_message = user_name + " entrou."
     post_message(thread_sock,user_message)
+
+    ######################################
+    # Laço para receber mensagens do cliente, tratá-las
+    # e enviá-las para os demais clientes.
     while (True):
         msg, cliente = thread_sock.recvfrom(1024)
         user_message = user_name + ": " + msg.decode()
         post_message(thread_sock,user_message)
+    #
+    ######################################
+#
+###################################
 
 
-
-
-#INICIO DO PROGRAMA           
-PORT = 5000            # Porta que o Servidor esta
+###################################
+# INICIO DO PROGRAMA 
+#
+###################################
+# Inicialização da lista de clientes conectados
+# e do socket para a conexão de novos clientes.
+#
 clientes = []
-
+HOST = input("Enter host address: ")
+PORT = 5000            # Porta que o Servidor esta
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 orig = (HOST, PORT)
 udp.bind(orig)      #Cria Socket no host e porta especificado para receber
                     #conexões de clientes.
+###################################
 
+###################################
+# 
+# Laço para receber novos clientes,
+# sempre que um novo cliente é conectado,
+# é criado uma thread para tratá-lo
+# e o cliente é inserido na lista de
+# clientes conectados.
+#
 while True:
     #Servidor fica em loop infinito esperando novas conexões, cada
     #vez que o servidor recebe HELLO de um cliente, ele cria uma thread
@@ -83,4 +127,5 @@ while True:
         _thread.start_new_thread(Cliente_Thread,(cliente,))
     else:
         pass
-
+#
+##################################
